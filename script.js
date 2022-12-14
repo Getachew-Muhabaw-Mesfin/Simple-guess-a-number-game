17ef165792f04f1e578bb905c7834c78fb4b4a2f
Getachew Muhabaw <getachewmuhabaw@gmail.com>
2022-12-14 16:15:19 +0300

    Store high Score logic

--- script.js
+++ script.js
@@ -19,24 +19,37 @@ document.querySelector('.check').addEventListener('click',clickCheck);
 The same as below 🤪
 */
 
-const secretNumber= Math.trunc(Math.random()*20)+1;
+let secretNumber= Math.trunc(Math.random()*20)+1;
+console.log(`Your secret Number is: ${secretNumber}`);
 let score =20;
+let highScore=0;
 
 // function wrongGuess(){
 //     score--;
 //     document.querySelector('.score').textContent=score;
 // }
-document.querySelector('.number').textContent=secretNumber;
+// document.querySelector('.number').textContent=secretNumber;
 document.querySelector('.check').addEventListener('click',function(){
     const guess= Number(document.querySelector('.guess').value);
     // console.log(guess, typeof guess);
     // emgi pallate for windows windows key +. 
+
+    //No number is entered
     if(!guess){
         document.querySelector('.message').textContent='⛔ No number! '
     }
+    //The player guess correctly
     else if(guess===secretNumber){
-        document.querySelector('.message').textContent="🌽You get the correct Number!!"
-    }
+        document.querySelector('.number').textContent=secretNumber;
+        document.querySelector('.message').textContent="🌽You get the correct Number!!";
+        document.querySelector('body').style.backgroundColor='#60b347';
+        document.querySelector('.number').style.width="30rem";
+        if(score>highScore){
+            highScore=score;
+            document.querySelector('.highscore').textContent=highScore;
+        }
+        }
+    //When guess is to high
     else if(guess>secretNumber){
         if(score>1){
         document.querySelector('.message').textContent="📈 Too high!";
@@ -49,6 +62,7 @@ document.querySelector('.check').addEventListener('click',function(){
         }
        
     }
+    // when guess is to loow
     else if(guess<secretNumber){
         if(score>1){
     document.querySelector('.message').textContent="📉 Too Low!";
@@ -61,4 +75,17 @@ document.querySelector('.check').addEventListener('click',function(){
         }
 
 }
+})
+
+//Functionality of Agian button 
+
+document.querySelector('.again').addEventListener('click',function(){
+    secretNumber= Math.trunc(Math.random()*20)+1;
+    score=0;
+    document.querySelector('.score').textContent=score;
+    document.querySelector('.message').textContent="Start Guessing....";
+    document.querySelector('.guess').value=''
+    document.querySelector('body').style.backgroundColor="#222";
+    document.querySelector('.number').textContent="?";  
+    document.querySelector('.number').style.width="15rem" ;
 })
